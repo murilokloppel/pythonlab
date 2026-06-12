301300a3058cb54a38b96cf8c0e66101c12fa144
Murilo Kloppel <murilo.kloppel@outlook.com>
2026-06-12 15:39:16 -0300

    first scrap

--- meu_scraper.py
+++ meu_scraper.py
@@ -1,2 +1,13 @@
+import requests
 from bs4 import BeautifulSoup
 
+url = "https://lojabrunamodas.com/?utm_source=chatgpt.com"
+headers = {"User-Agent": "Mozilla/5.0"}
+
+resposta = requests.get(url, headers=headers)
+soup = BeautifulSoup(resposta.text, 'html.parser')
+
+precos = soup.select('span.price.price--highlight')
+
+for p in precos:
+    print(p.text.strip())
