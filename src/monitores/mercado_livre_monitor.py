a7b5289af585625ad8f596749c91c687bec7c3ad
Murilo Kloppel <murilo.kloppel@outlook.com>
2026-06-16 17:28:34 -0300

    aplicando_tempo_espera

--- src/monitores/mercado_livre_monitor.py
+++ src/monitores/mercado_livre_monitor.py
@@ -1,9 +1,22 @@
+import time
+import random
 from src.monitores.monitor_base import MonitorBase
+
 
 class MercadoLivreMonitor(MonitorBase):
     def __init__(self):
         super().__init__()
+        self.page.set_extra_http_headers({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        })
 
-    def executar(self):
-        print("Acessando Mercado Livre...")
-        self.page.goto("https://www.mercadolivre.com.br")
+    def _espera_humana(self, min_seg=10, max_seg=20):
+        tempo = random.uniform(min_seg, max_seg)
+        time.sleep(tempo)
+
+    def executar(self, lista_produtos):
+        for produto in lista_produtos:
+            busca_url = f"https://lista.mercadolivre.com.br/{produto.replace(' ', '-')}"
+            self.page.goto(busca_url)
+
+            self._espera_humana(12, 18)
--- src/monitores/monitor_base.py
+++ src/monitores/monitor_base.py
@@ -7,9 +7,17 @@ class MonitorBase:
         self.browser = self.playwright.chromium.launch(headless=False)
         self.page = self.browser.new_page()
 
+    def iniciar(self):
+        if self.playwright is None:
+            self.playwright = sync_playwright().start()
+            self.browser = self.playwright.chromium.launch(headless=self.headless)
+            self.page = self.browser.new_page()
+
     def executar(self):
         raise NotImplementedError("As subclasses devem implementar o método 'executar'")
 
     def fechar(self):
-        self.browser.close()
-        self.playwright.stop()
+        if self.browser:
+            self.browser.close()
+        if self.playwright:
+            self.playwright.stop()
